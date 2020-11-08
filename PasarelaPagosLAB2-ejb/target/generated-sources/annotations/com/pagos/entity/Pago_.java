f6e9fe9e04489f796b66339c11eabcd8d7a462fb
Andrés F. Castro <felimon0619@hotmail.com>
2020-11-08 16:30:47 -0500

    Pasarela Pagos

---
+++ PasarelaPagosLAB2-ejb/target/generated-sources/annotations/com/pagos/entity/Pago_.java
@@ -0,0 +1,23 @@
+package com.pagos.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-08T15:54:45")
+@StaticMetamodel(Pago.class)
+public class Pago_ { 
+
+    public static volatile SingularAttribute<Pago, String> nomCliente;
+    public static volatile SingularAttribute<Pago, String> emailCliente;
+    public static volatile SingularAttribute<Pago, String> expiracionTarj;
+    public static volatile SingularAttribute<Pago, String> idCliente;
+    public static volatile SingularAttribute<Pago, String> csvTar;
+    public static volatile SingularAttribute<Pago, Integer> idPago;
+    public static volatile SingularAttribute<Pago, String> numTar;
+    public static volatile SingularAttribute<Pago, Double> valorPago;
+    public static volatile SingularAttribute<Pago, String> tipoTar;
+    public static volatile SingularAttribute<Pago, String> apellCliente;
+    public static volatile SingularAttribute<Pago, String> fechaPago;
+
+}
