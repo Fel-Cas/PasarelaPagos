5f91b27984f4d6667639752fed3b4f1e4e20205f
Andrés F. Castro <felimon0619@hotmail.com>
2020-11-09 21:04:18 -0500

    Laboratorio Funcionando

--- PasarelaPagosLAB2-ejb/target/generated-sources/annotations/com/pagos/entity/Pago_.java
+++ PasarelaPagosLAB2-ejb/target/generated-sources/annotations/com/pagos/entity/Pago_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-08T15:54:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-09T19:14:41")
 @StaticMetamodel(Pago.class)
 public class Pago_ { 
 
